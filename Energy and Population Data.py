909909fbfb73df593d40ed81643df87dd4372518
Megan Jenney <megan.jenney@tufts.edu>
2019-04-21 16:08:11 -0400

    finished creating list for energy usage per person

--- Energy and Population Data.py
+++ Energy and Population Data.py
@@ -3,25 +3,33 @@ import numpy as np
 
 def createEnergyandCityLists():
     cities = []
-    print(cities)
     city_energy = []
     pop = []
-    file = open("Monthly-Electricity-Consumption-for-Major-US-Cities.xlsx")
-    print(file)
-    """split_character = ';'
+    energy_per_person = []
+    file = open("Monthly-Electricity-Consumption-for-Major-US-Cities.csv")
+    split_character = ','
     for line in file:
         data_line = line.split(split_character)  # split each line
-        #print(data_line)
         cities.append(data_line[0])  # add first element, name of city, in order
-        city_energy.append(data_line[1])  # add second element, energy usage of city (in mega watts), in order
-        pop.append(data_line[2])  # add third element, population (millions of people), in order
+        city_energy.append(float(data_line[1]))  # add second element, energy usage of city (in gigawatts), in order
+        data_line[2] = data_line[2].strip("\n")  # get rid of \n character on end of each element
+        pop.append(float(data_line[2]))  # add third element, population (millions of people), in order
     file.close()
-    city_energy_array = np.asarray(city_energy)
-    pop_array = np.asarray(pop)
-    energy_per_person_array = np.divide(city_energy_array, pop_array)
-    energy_per_person = np.array(energy_per_person_array.tolist())
-    #print(energy_per_person)"""
-    return cities #, city_energy, pop, energy_per_person
+    print("cities list is: "+str(cities))
+    print("city energy list is: "+str(city_energy))
+    print("pop list is: "+str(pop))
+    i = 0  # counter
+    while i < len(city_energy):
+        energy = city_energy[i]
+        population = pop[i]*100000  # gives exact population
+        en_use_per_person = (energy/population)*1000  # gives energy use per person in megawatts
+        energy_per_person.append(en_use_per_person)
+        i += 1
+    print("energy per person list is: "+str(energy_per_person))
+    return cities, city_energy, pop, energy_per_person
     
 createEnergyandCityLists()
-#def graphEnergyData(cities, , ):
+
+def graphEnergyData(cities, city_energy, energy_per_person):
+    
+    return
