520b2ed59199f53b8a187257ea4379938c04856e
BrettSilv12 <brett.silv12@gmail.com>
2019-04-24 14:55:16 -0400

    Update ReadDataFinal.py

--- ReadDataFinal.py
+++ ReadDataFinal.py
@@ -19,11 +19,13 @@ def readData():
         Latitude = np.zeros((total_row,))
         Longitude = np.zeros((total_row,))
         Population = np.zeros((total_row,))
+        B_T_U = np.zeros((total_row,))
         for row in reader:
             Cities.append(row[0])
             Latitude[index] = ((float(row[1]) - 25)/(47 - 25))
             Longitude[index] = ((float(row[2]) - 71)/(122 - 71))
             Population[index] = (float(row[3]) * (10 ** 6))
+            B_T_U[index] = ((302000000 / 12) * (Population[index] * (10 ** 3)))
             line_count += 1
             index += 1
         Longitude = Longitude * (-1)
@@ -35,9 +37,11 @@ def readData():
         print(Longitude)
         print("Population: ")
         print(Population)
-    return (Cities, Latitude, Longitude, Population)
+        print("Monthly Btu per City: ")
+        print(B_T_U)
+    return (Cities, Latitude, Longitude, Population, B_T_U)
         
-Cities, Latitude, Longitude, Population = readData()
+Cities, Latitude, Longitude, Population, B_T_U = readData()
 
 test_lat = input("What is the latitude of the point you want to test? (Choose a point between 25 and 47) ")
 test_lat = ((float(test_lat) - 25)/(47-25))
@@ -54,4 +58,3 @@ def graphData(Latitude, Longitude, test_lon, test_lat):
     plt.show()
     
 graphData(Latitude, Longitude, test_lon, test_lat)
-
